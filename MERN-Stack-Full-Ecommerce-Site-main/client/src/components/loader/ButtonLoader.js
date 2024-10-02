b0799966ea39c2b646f0547bd82d6c0427dd622e
sarthak0777 <109307888+sarthak0777@users.noreply.github.com>
2024-10-02 23:25:11 +0530

    delete

--- MERN-Stack-Full-Ecommerce-Site-main/client/src/components/loader/ButtonLoader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ButtonLoader.scss";
-
-const ButtonLoader = () => {
-    return <div className="lds-dual-ring"></div>;
-};
-
-export default ButtonLoader;
--- MERN-Stack-Full-Ecommerce-Site-main/client/src/config.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL:
-        "https://e-shop-app.onrender.com",
-});
